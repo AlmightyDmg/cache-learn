1e5c5e0fab385c125fbb07e61670399154aa9ca0
duanxiaoyi <duanxiaoyi@haizhi.com>
2021-07-01 07:27:02 +0800

    bugfix: 解决checkstyle和编译问题。

--- dataclient/src/main/java/com/haizhi/dataclient/connection/dmc/client/tassadar/request/MergeTbFileReq.java
+++ dataclient/src/main/java/com/haizhi/dataclient/connection/dmc/client/tassadar/request/MergeTbFileReq.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
--- dataclient/src/main/java/com/haizhi/dataclient/connection/dmc/client/tassadar/response/MergeTbFileResp.java
+++ dataclient/src/main/java/com/haizhi/dataclient/connection/dmc/client/tassadar/response/MergeTbFileResp.java
@@ -1,12 +1,10 @@
 package com.haizhi.dataclient.connection.dmc.client.tassadar.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class MergeTbFileResp {
