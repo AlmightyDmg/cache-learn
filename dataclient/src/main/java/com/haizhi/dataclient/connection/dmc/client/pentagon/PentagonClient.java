de68fc8c960f59eb3d4d85ab0fbd8f5341caa7b6
duanxiaoyi <duanxiaoyi@haizhi.com>
2021-07-01 08:48:09 +0800

    bugfix: 解决checkstyle问题。

--- dataclient/src/main/java/com/haizhi/dataclient/connection/dmc/client/pentagon/PentagonClient.java
+++ dataclient/src/main/java/com/haizhi/dataclient/connection/dmc/client/pentagon/PentagonClient.java
@@ -6,9 +6,7 @@ import javax.validation.constraints.NotNull;
 
 import java.util.List;
 
-import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.JsonBeanParam;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
