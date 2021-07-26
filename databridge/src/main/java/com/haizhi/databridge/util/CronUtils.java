bd401662a129dcbc9591697d6720cf4387e33404
duanxiaoyi <duanxiaoyi@haizhi.com>
2021-07-26 16:54:47 +0800

    bugfix: 创建导出任务失败。

--- databridge/src/main/java/com/haizhi/databridge/util/CronUtils.java
+++ databridge/src/main/java/com/haizhi/databridge/util/CronUtils.java
@@ -1,7 +1,13 @@
 package com.haizhi.databridge.util;
 
+import org.springframework.util.ObjectUtils;
+
 public class CronUtils {
     public static String toQuartsCron(String linuxCron) {
+        if (ObjectUtils.isEmpty(linuxCron)) {
+            return linuxCron;
+        }
+
         String linuxCronNoSpace = linuxCron.trim();
         char lastChar = linuxCronNoSpace.charAt(linuxCronNoSpace.length()-1);
         if (lastChar == '*') {
